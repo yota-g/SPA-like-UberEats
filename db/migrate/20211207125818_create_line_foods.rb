d2cff17ffa7631fb0d0ca5c3bbc26d508abaf0e9
yota-g <yota18493@gmail.com>
2021-12-07 22:10:56 +0900

    modified create line foods's order column

--- db/migrate/20211207125818_create_line_foods.rb
+++ db/migrate/20211207125818_create_line_foods.rb
@@ -3,7 +3,7 @@ class CreateLineFoods < ActiveRecord::Migration[6.0]
     create_table :line_foods do |t|
       t.references :food, null: false, foreign_key: true
       t.references :restaurant , null: false, foreign_key: true
-      t.references :order, null: false, foreign_key: true
+      t.references :order,  foreign_key: true
       t.integer :count, null: false, default: 0
       
       t.timestamps
