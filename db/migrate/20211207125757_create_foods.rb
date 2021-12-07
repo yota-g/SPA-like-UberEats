2441ee99039f09b07833d10545b0532b20b09f69
yota-g <yota18493@gmail.com>
2021-12-07 22:03:28 +0900

    create migration

---
+++ db/migrate/20211207125757_create_foods.rb
@@ -0,0 +1,6 @@
+class CreateFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :foods do |t|
+    end
+  end
+end
---
+++ db/migrate/20211207125818_create_line_foods.rb
@@ -0,0 +1,6 @@
+class CreateLineFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :line_foods do |t|
+    end
+  end
+end
