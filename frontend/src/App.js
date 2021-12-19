99977251b08370b19e77eb8e0e1cd4547d31a1e7
yota-g <yota18493@gmail.com>
2021-12-19 11:41:47 +0900

    modified Routes to Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//  SwitchをRoutesに変更
 import './App.css';
+
+import {Restaurants} from './containers/Restaurants.jsx';
+import {Foods} from './containers/Foods.jsx';
+import {Orders} from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          テキストを変更してみる。
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* 店舗一覧ページ */}
+        <Route exact path="/restaurants">
+          <Restaurants />
+        </Route>
+        {/* フード一覧ページ */}
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) => <Foods match={ match } />}
+        />
+        {/* オーダーページ */}
+        <Route exact path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
