b5d2514eaac243439960315df666eba57b0aebce
yota-g <yota18493@gmail.com>
2021-12-19 11:40:46 +0900

    modified jsx file return

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,12 @@
 import React, {Fragment} from "react";
 
-export const Foods = () => {
+export const Foods = ({
+  match
+}) => {
   return (
     <Fragment>
       フード一覧
+      <p>restaurantId は{match.params.restaurantsId}です。</p>
     </Fragment>
   )
 }
