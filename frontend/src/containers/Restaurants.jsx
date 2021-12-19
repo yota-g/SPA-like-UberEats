9491c524b49a8ed8afe9e6ebf58a9910ccfdf94d
yota-g <yota18493@gmail.com>
2021-12-19 23:48:35 +0900

    create api file

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,13 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+//api
+import {fetchRestaurants} from '../apis/restaurants';
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) => 
+    console.log(data))
+  },[])
   return (<Fragment>レストラン一覧</Fragment>)
 }
