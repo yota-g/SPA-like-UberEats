542119921611adfb821bd786c2d05f5f07c77e79
yota-g <yota18493@gmail.com>
2021-12-27 22:31:13 +0900

    Foods styled-components created

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,5 +1,11 @@
 import React, { Fragment, useEffect, useReducer } from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../style_constants';
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
 
+//reducers
 import {
   initialState as foodsInitialState,
   foodsActionTyps,
@@ -11,6 +17,10 @@ import { fetchFoods } from '../apis/foods';
 
 //constants
 import { REQUEST_STATE } from '../constants';
+
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
 
 export const Foods = ({ match }) => {
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
@@ -28,13 +38,66 @@ export const Foods = ({ match }) => {
   }, []);
   return (
     <Fragment>
-      {foodsState.fetchState === REQUEST_STATE.LOADING ? (
-        <Fragment>
-          <p>ロード中...</p>
-        </Fragment>
-      ) : (
-        foodsState.foodsList.map((food) => <div key={food.id}>{food.name}</div>)
-      )}
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {foodsState.fetchState === REQUEST_STATE.LOADING ? (
+          <Fragment>
+            {[...Array(12).keys()].map((i) => (
+              <ItemWrapper key={i}>
+                <Skeleton key={i} variant="rect" width={450} height={180} />
+              </ItemWrapper>
+            ))}
+          </Fragment>
+        ) : (
+          foodsState.foodsList.map((food) => (
+            <ItemWrapper key={food.id}>
+              <FoodWrapper
+                food={food}
+                onClickFoodWrapper={(food) => console.log(food)}
+                imageUrl={FoodImage}
+              />
+            </ItemWrapper>
+          ))
+        )}
+      </FoodsList>
     </Fragment>
   );
 };
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  paddin: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
